2cd7c1ad7dcf1606bb4c528db0045c78053e73a8
a.blishch <alnibl88@gmail.com>
2024-06-26 12:52:46 +0300

    Fix condition to handle None return in frame detection and tracking

--- src/app.py
+++ src/app.py
@@ -39,50 +39,54 @@ def model_detect_track_run(frame):
 
         for result in results:
             boxes = result.boxes  # Boxes object for bounding box outputs
-            bboxes_ = boxes.xyxy.tolist()
-            bboxes = list(map(lambda x: list(map(lambda y: int(y), x)), bboxes_))
-            # на будущее нужно обработать случай, когда сбивается трэк
-            track_ids = results[0].boxes.id.int().cpu().tolist()
+            if boxes is not None and boxes.id is not None:
+                bboxes_ = boxes.xyxy.tolist()
+                bboxes = list(map(lambda x: list(map(lambda y: int(y), x)), bboxes_))
+                # на будущее нужно обработать случай, когда сбивается трэк
+                track_ids = boxes.id.int().cpu().tolist()
 
-            confs_ = boxes.conf.tolist()
-            confs = list(map(lambda x: int(x * 100), confs_))
-            classes_ = boxes.cls.tolist()
-            classes = list(map(lambda x: int(x), classes_))
-            cls_dict = result.names
-            class_names = list(map(lambda x: cls_dict[x], classes))
+                confs_ = boxes.conf.tolist()
+                confs = list(map(lambda x: int(x * 100), confs_))
+                classes_ = boxes.cls.tolist()
+                classes = list(map(lambda x: int(x), classes_))
+                cls_dict = result.names
+                class_names = list(map(lambda x: cls_dict[x], classes))
 
-            print(f"bboxes: {bboxes}")
-            print(f"track_ids: {track_ids}")
-            print(f"class_names: {class_names}, confs: {confs}")
+                print(f"bboxes: {bboxes}")
+                print(f"track_ids: {track_ids}")
+                print(f"class_names: {class_names}, confs: {confs}")
 
-            for bbox, conf, class_name, id in zip(bboxes, confs, class_names, track_ids):
-                x1, y1, x2, y2 = bbox
-                # Отрисовка bounding box
-                cv2.rectangle(img_, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                text = f"id {id}, {class_name} {conf}"
-                # Отрисовка текста
-                cv2.putText(img_, text, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
+                for bbox, conf, class_name, id in zip(bboxes, confs, class_names, track_ids):
+                    x1, y1, x2, y2 = bbox
+                    # Отрисовка bounding box
+                    cv2.rectangle(img_, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                    text = f"id {id}, {class_name} {conf}"
+                    # Отрисовка текста
+                    cv2.putText(img_, text, (x1, y1 - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 1)
 
-                # Вырезание части изображения, которую обводит bounding box
-                cropped_img = frame[y1:y2, x1:x2]
-                # Уменьшение размера вырезанной части до миниатюры
-                thumbnail = cv2.resize(cropped_img, thumbnail_size)
+                    # Вырезание части изображения, которую обводит bounding box
+                    cropped_img = frame[y1:y2, x1:x2]
+                    # Уменьшение размера вырезанной части до миниатюры
+                    thumbnail = cv2.resize(cropped_img, thumbnail_size)
 
-                # Определение координат для размещения миниатюры
-                thumbnail_y1 = start_y
-                thumbnail_y2 = start_y + thumbnail_size[1]
-                thumbnail_x1 = start_x
-                thumbnail_x2 = start_x + thumbnail_size[0]
+                    # Определение координат для размещения миниатюры
+                    thumbnail_y1 = start_y
+                    thumbnail_y2 = start_y + thumbnail_size[1]
+                    thumbnail_x1 = start_x
+                    thumbnail_x2 = start_x + thumbnail_size[0]
 
-                # Размещение миниатюры на оригинальном изображении
-                img_[thumbnail_y1:thumbnail_y2, thumbnail_x1:thumbnail_x2] = thumbnail
+                    # Размещение миниатюры на оригинальном изображении
+                    img_[thumbnail_y1:thumbnail_y2, thumbnail_x1:thumbnail_x2] = thumbnail
 
-                # Смещение для следующей миниатюры
-                start_y += thumbnail_size[1] + thumbnail_offset
-            # сохранить кадр + миниатюра
-            #cv2.imwrite('data/1_.jpg', img_)
-        print(f"Прошло: {round(time.time() - start_time, 3)} сек.")
-        return img_
+                    # Смещение для следующей миниатюры
+                    start_y += thumbnail_size[1] + thumbnail_offset
+                # сохранить кадр + миниатюра
+                #cv2.imwrite('data/1_.jpg', img_)
+                print(f"Прошло: {round(time.time() - start_time, 3)} сек.")
+                return img_
+            else:
+                print(f"Прошло: {round(time.time() - start_time, 3)} сек.")
+                return None
 
 def generate_frames():
     cap = cv2.VideoCapture(SOURCE_VIDEO_PATH)  # Индекс 0 для первой камеры
@@ -95,10 +99,12 @@ def generate_frames():
             break
         else:
             frame_ = model_detect_track_run(frame)# тут детекция, трекинг и обработка
-            ret, buffer = cv2.imencode('.jpg', frame_)  # Кодирование кадра в JPEG
-            frame_ = buffer.tobytes()
+            if frame_ is not None:
+                frame = frame_
+            ret, buffer = cv2.imencode('.jpg', frame)  # Кодирование кадра в JPEG
+            frame = buffer.tobytes()
             yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame_ + b'\r\n')
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
